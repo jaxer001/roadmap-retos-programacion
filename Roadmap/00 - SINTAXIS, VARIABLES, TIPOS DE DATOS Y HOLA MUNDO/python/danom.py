cef3bb0257e0e2e0623fb4e687a3cdfcd0bcf432
jaxer001 <162832279+jaxer001@users.noreply.github.com>
2024-03-09 12:25:59 -0800

    Create danom.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/danom.py
@@ -0,0 +1,25 @@
+#https://www.python.org/#
+
+#line of comment#
+
+"""
+another line of comment
+"""
+
+my_variable="pepsi"
+my_other_variable="fanta"
+print(my_other_variable)
+
+#int
+4
+my_int=4
+
+#float
+1.5
+
+#tupla   #su valor no cambia#
+0,1,2,3
+
+my_bool=true
+
+print("Hola,python")
